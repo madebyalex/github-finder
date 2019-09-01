d39c6b229fbf59e5c6f5241908bd3502cf40111b
Alexander Nuzhnyi <hello@alexander.works>
2019-09-01 23:05:10 +0300

    Initial version

---
+++ src/components/users/User.js
@@ -0,0 +1,126 @@
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+  };
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      company,
+      location,
+      bio,
+      blog,
+      login,
+      hireable,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists
+    } = this.props.user;
+
+    const { loading, repos } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt={name}
+              style={{ width: '120px' }}
+            />
+
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github profile
+            </a>
+
+            <ul>
+              {login && (
+                <Fragment>
+                  <li>
+                    <strong>Username: </strong>
+                    {login}
+                  </li>
+                </Fragment>
+              )}
+
+              {company && (
+                <Fragment>
+                  <li>
+                    <strong>Company: </strong>
+                    {company}
+                  </li>
+                </Fragment>
+              )}
+
+              {blog && (
+                <Fragment>
+                  <li>
+                    <strong>Portfolio: </strong>
+                    <a
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      href={'http://' + blog}
+                    >
+                      {blog}
+                    </a>
+                  </li>
+                </Fragment>
+              )}
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
+      </Fragment>
+    );
+  }
+}
+
+export default User;
